cfa8bbb4cd48bd7d2a47e85e7f6a2f4da5bb2739
EnMaj <yakimova.tonya@gmail.com>
2023-03-16 01:06:26 +0700

    test2+ru_local

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import ru_local as ru
 
-day = int(input())
+day = int(input(ru.DAY))
 
-mil_1=16637000000 #миль
-v=38241 #миль в час
+mil_1 = 16637000000 #миль
+v = 38241 #миль в час
 
-mil_2= mil_1-day*24*v
+mil_2 = mil_1-day*24*v
+km_2 = mil_2*1.609
+ae = km_2/149597870.7
+delay_comm = ((km_2*1000/299792458)/60)/60
 
-km_2=mil_2*1.609
-
-ae=km_2/149597870.7
-
-print(mil_2)
-print(km_2)
-print(ae)
+print(ru.DISTANCE)
+print(mil_2, ru.MIL)
+print(km_2, ru.KM)
+print(ae, ru.AE)
+print(ru.DELAY,delay_comm,ru.HOUR)
